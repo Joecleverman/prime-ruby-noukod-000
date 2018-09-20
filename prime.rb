74e9607dc6973af7f8722cd320b6277a0edccc8d
Josue Abraham <josuea@noukod.com>
2018-09-20 17:25:36 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -3,8 +3,8 @@ def prime?(integer)
   a = 2
   if integer > 1
     range = (a..integer-1).to_a
-    range.none? do |num| 
-                                
+    range.none? do |num|
+
       integer % num == 0
     end
   else
